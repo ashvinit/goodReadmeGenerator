ecca1fa0a1a31fc34dd0bce152aa774f3863167d
Ashvini Thomas <ashvinithomas@gmail.com>
2020-11-18 18:54:30 -0500

    added questions to the questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,51 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'projectname',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What command should be run to install dependencies?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be run to run tests?'
+    },
+    {
+        type: 'input',
+        name: 'using',
+        message: 'What does the user need to know about using the repo?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?'
+    }
 ];
 
 // function to write README file
